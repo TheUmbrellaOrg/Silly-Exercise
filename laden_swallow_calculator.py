c4ead4ca88961ef993d667c82509189936fece22
hakon98 <61887422+hakon98@users.noreply.github.com>
2022-09-15 14:02:10 +0200

    Added function swallow_calc. Added conditional output string.

--- laden_swallow_calculator.py
+++ laden_swallow_calculator.py
@@ -1,6 +1,19 @@
 # https://www.youtube.com/watch?v=liIlW-ovx0Y
 
-coconut = 454
-swallow_capacity = 20
+"""
+Function who calculates the amount of swallows needed.
+Includes a conditional output string.
+"""
 
-print(coconut / swallow_capacity)
+
+def swallow_calc(coconut=454, swallow_capacity=20):
+    return coconut / swallow_capacity
+
+
+coconut = int(input('Coconut weight: '))
+swallows = int(input('Swallow Capacity: '))
+result = swallow_calc(coconut, swallows)
+
+print(f"It would require at least "
+      f"{result} {'swallows' if result > 1 else 'swallow'}"
+      f" to carry a {coconut}g coconut.")
